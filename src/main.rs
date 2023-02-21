f8c2f0deea1b245a555c56861a3154c2e8d22b31
Minwoo Jeong <jeongmwj@gmail.com>
2023-02-21 19:51:04 +0900

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: rox [script]");
+        process::exit(64);
+    } else if args.len() == 2 {
+        // TODO: Implement runFile
+        println!("Run the file");
+    } else {
+        // TODO: Implement runPrompt
+        println!("Run the prompt");
+    }
 }
