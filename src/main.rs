0047a5337b96a5b7dc5ad9bcc7b23c5f6a3cf65f
Minwoo Jeong <jeongmwj@gmail.com>
2023-02-21 21:05:45 +0900

    Implement `run_prompt` function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::env;
 use std::error::Error;
 use std::fs;
+use std::io;
+use std::io::Write;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    // TODO: error handling
     if args.len() > 2 {
         println!("Usage: rox [script]");
         process::exit(64);
     } else if args.len() == 2 {
         run_file(&args[1]);
     } else {
-        // TODO: Implement runPrompt
-        println!("Run the prompt");
+        run_prompt();
     }
 }
 
@@ -20,6 +23,16 @@ fn run_file(path: &String) -> Result<(), Box<dyn Error>> {
     let source = fs::read_to_string(path)?;
     run(&source);
     Ok(())
+}
+
+fn run_prompt() -> Result<(), Box<dyn Error>> {
+    loop {
+        print!("ROX> ");
+        io::stdout().flush()?;
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+        run(&line);
+    }
 }
 
 fn run(source: &String) {
