d58aa00097e02dd403947d244001491962cdc8b8
Minwoo Jeong <jeongmwj@gmail.com>
2023-02-21 20:23:05 +0900

    Implement `run_file` function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::error::Error;
+use std::fs;
 use std::process;
 
 fn main() {
@@ -7,10 +9,19 @@ fn main() {
         println!("Usage: rox [script]");
         process::exit(64);
     } else if args.len() == 2 {
-        // TODO: Implement runFile
-        println!("Run the file");
+        run_file(&args[1]);
     } else {
         // TODO: Implement runPrompt
         println!("Run the prompt");
     }
+}
+
+fn run_file(path: &String) -> Result<(), Box<dyn Error>> {
+    let source = fs::read_to_string(path)?;
+    run(&source);
+    Ok(())
+}
+
+fn run(source: &String) {
+    println!("Source: {source}");
 }
